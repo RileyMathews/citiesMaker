c28fafe0e7e74f347073b11d75253273513e0dd4
Riley Mathews <rileymathews80@gmail.com>
2018-04-13 12:48:10 -0500

    changed from using divs to sections

--- printer.js
+++ printer.js
@@ -5,7 +5,7 @@ const article = document.querySelector("#article")
 
 /*
     Structure of city components...
-    <div class="continent">
+    <section class="continent">
         <h3>CITY</h3>
         <img src=database.cities[i].image>
         <p>YEAR VISISTED</p>
@@ -13,28 +13,28 @@ const article = document.querySelector("#article")
             <li>attraction 1</li>
             ...
         </ul>
-    </div>
+    </section>
 */
 
 database.cities.forEach(city => {
     console.log(city.name)
-    let div = document.createElement('div')
+    let section = document.createElement('section')
 
     //create heading
     let heading = document.createElement('h3')
     heading.textContent = city.name
-    div.appendChild(heading)
+    section.appendChild(heading)
 
     //create image
     let img = document.createElement('img')
     img.setAttribute('src', city.image)
     img.setAttribute('height', '200')
-    div.appendChild(img)
+    section.appendChild(img)
 
     //create year visited element
     let p = document.createElement('p')
     p.textContent = 'Visited in: ' + city.visited
-    div.appendChild(p)
+    section.appendChild(p)
 
     //create list element
     let list = document.createElement('ul')
@@ -43,12 +43,12 @@ database.cities.forEach(city => {
         item.textContent = element
         list.appendChild(item)
     });
-    div.appendChild(list)
+    section.appendChild(list)
 
 
 
 
 
     //add finished city component to article
-    article.appendChild(div)
+    article.appendChild(section)
 });
